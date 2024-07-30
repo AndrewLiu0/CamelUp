dba7f7952bf5f5bbdd2533111e0612cbc73ced5a
an6reww <an6reww@gmail.com>
2024-07-30 10:54:58 -0400

    reorganize

--- main.py
+++ main.py
@@ -4,4 +4,41 @@ from display import Display
 if __name__ == "__main__":
     gameState = GameState()
     display = Display(gameState)
-    display.game_display()
+    display.game_display(gameState)
+    turn = 0
+    while len(gameState.tent.dices) > 0:
+        move = ""
+        while move != "B" and move != "R":
+            if turn % 2 == 0:
+                move = input("p1 - (B)et or (R)oll? ")
+                print()
+            else:
+                move = input("p2 - (B)et or (R)oll? ")
+                print()
+        if move == "R":
+            roll = gameState.tent.roll()
+            gameState.movement(roll[0], roll[1])
+            gameState.player_scores[turn % 2] += 1
+        else:
+            bet = ""
+            length = 1
+            while True:
+                bet = input("What color do you want to bet on? ")
+                if bet in gameState.colors:
+                    if gameState.bet(turn % 2, bet):
+                        break
+                print()
+        display.game_display(gameState)
+        turn += 1
+    scores = gameState.pay_out()
+    first = "🥇"
+    second = "🥈"
+    print()
+    if scores[0] > scores[1]:
+        print(f"p1 comes in 1st with {scores[0]} coins {first} {first} {first}!")
+        print(f"p2 comes in 2nd with {scores[1]} coins {second} {second} {second}!")
+    elif scores[1] > scores[0]:
+        print(f"p2 comes in 1st with {scores[1]} coins {first} {first} {first}!")
+        print(f"p1 comes in 2nd with {scores[0]} coins {second} {second} {second}!")
+    else:
+        print(f"p1 and p2 tie {first} {first} {first}!")
--- tent.py
+++ tent.py
@@ -1,5 +1,8 @@
 
 import random
+from colorama import *
+init(autoreset=True)
+
 '''
 Used if player chooses to roll the dice
 '''
@@ -10,12 +13,44 @@ class Tent:
         red = Dice("Red")
         blue = Dice("Blue")
         purple = Dice("Purple")
-        self.dices = {green, yellow, red, blue, purple}
+        self.dices = [green, yellow, red, blue, purple]
+        self.rolls = []
     
     # def roll 
+    def roll(self):
+        dice = random.choice(self.dices)
+        color = dice.name
+        roll = dice.getRandomNumber()
+        self.dices.remove(dice)
+        self.rolls.append(dice)
+        return (color, roll)
+
+    def __str__(self):
+        ans = ""
+        for dice in self.rolls:
+            if dice.name == "Green":
+                if dice.number != None:
+                    ans += Back.GREEN + str(dice.number)
+            elif dice.name == "Yellow":
+                if dice.number != None:
+                    ans += Back.YELLOW + str(dice.number)
+            elif dice.name == "Red":
+                if dice.number != None:
+                    ans += Back.RED + str(dice.number)
+            elif dice.name == "Blue":
+                if dice.number != None:
+                    ans += Back.BLUE + str(dice.number)
+            elif dice.name == "Purple":
+                if dice.number != None:
+                    ans += Back.MAGENTA + str(dice.number)
+            ans += Style.RESET_ALL + " "
+        return ans
 
 class Dice:
     def __init__(self, name) -> None:
         self.name = name
-    def getRandomNumber():
-        return random.randint(1,3)
+        self.number = None
+    def getRandomNumber(self):
+        value = random.randint(1,3)
+        self.number = value
+        return value
